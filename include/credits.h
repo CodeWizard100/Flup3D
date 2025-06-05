8ab89c4ae72ee85797dc1711c041e40fc8713ea0
Codewizard <132087862+CodeWizard100@users.noreply.github.com>
2025-06-05 20:15:17 +0200

    Update credits

--- include/credits.h
+++ include/credits.h
@@ -1,5 +1,5 @@
 /*
 Credits Of Flup3D
-Credits to thegabman for NMB (native message box) - repository: https://github.com/thegabman/native_message_box/
+Credits to thegabman for NMB (native message box) - repository: https://github.com/thegabman/native_message_box/ - Thanks for base, heavily modified
 
 */
